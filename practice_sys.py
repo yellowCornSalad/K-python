65de47b91eca1116586edd61d44249aab2f3630f
junho <zk836566@gmail.com>
2022-03-09 12:39:18 +0900

    added practice_random_module: printed local settings

---
+++ practice_sys.py
@@ -0,0 +1,18 @@
+# from time import random, randrange, choice
+
+# call module
+import sys
+
+# print call parameter(매개변수)
+print(sys.argv)
+print("-------------------------")
+
+# print local info.
+# print("get mac settings:()", sys.getwindowsversion())
+print("---")
+print("copyright:", sys.copyright)
+print("---")
+print("version:", sys.version)
+
+# exit program
+sys.exit()
