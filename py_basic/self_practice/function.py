db4a6e66b85ba77f068e8500e5287bc1dd96082d
junho <zk836566@gmail.com>
2022-03-13 23:57:24 +0900

    Update function.py
    changed science to computer

--- py_basic/self_practice/function.py
+++ py_basic/self_practice/function.py
@@ -7,7 +7,7 @@ students = [
 print("이름", "총점", "평균", sep = "\t")
 for student in students:
     # 점수의 총합과 평균 계산
-    score_sum = student["korean"] + student["math"] + student["english"] + student["science"]
+    score_sum = student["korean"] + student["math"] + student["english"] + student["computer"]
     score_average = score_sum / 4
     # 출력
     print(student["name"], score_sum, score_average, sep = "\t")
