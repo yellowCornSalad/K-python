1f75383402e298d76eaf2e33d38fb21fbd9bef78
junho <zk836566@gmail.com>
2022-03-14 00:17:16 +0900

    self studying {'python':'dictionary'}

---
+++ py_basic/self_practice/dic.py
@@ -0,0 +1,13 @@
+# https://wikidocs.net/72
+# 파이썬 딕셔너리 자습
+
+# dic이라는 이름의 비어있는 딕셔너리를 만든다.
+dic = {}
+dic ['dictionary'] = 'A reference book that contains the meaning of words'
+dic['python'] = 'Any of various nonvenomous snakes of the world'
+
+# print(dic['dictionary'])
+
+# 포켓용사전
+smalldic = {'dictionary' : 'reference', 'python': 'snake'}
+print(smalldic['python'])
