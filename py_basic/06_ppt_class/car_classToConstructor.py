4bd56b715e7d1a8621a5a886dccf1ddb74271ca6
junho <zk836566@gmail.com>
2022-03-14 10:24:40 +0900

    Create car_classToConstructor.py

---
+++ py_basic/06_ppt_class/car_classToConstructor.py
@@ -0,0 +1,12 @@
+# 클래스 선언
+class Car:
+    color = ""
+    speed = 0
+
+    def __init__(self, value1, value2):
+        self.color = value1
+        self.color = value2
+    
+# 메인 코드
+myCar1 = Car("빨강", 30)
+myCar2 = Car("파랑", 60)
