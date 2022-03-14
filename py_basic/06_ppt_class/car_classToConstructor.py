3cdb877ea3bf9b3cfe13bc1d733b8b3bf0693a3a
junho <zk836566@gmail.com>
2022-03-14 10:37:49 +0900

    Update car_classToConstructor.py

--- py_basic/06_ppt_class/car_classToConstructor.py
+++ py_basic/06_ppt_class/car_classToConstructor.py
@@ -1,12 +1,26 @@
 # 클래스 선언
+
+
 class Car:
     color = ""
     speed = 0
 
-    def __init__(self, value1, value2):
-        self.color = value1
-        self.speed = value2
-    
-# 메인 코드
-myCar1 = Car("빨강", 30)
-myCar2 = Car("파랑", 60)
+    def __init__(self, name, speed):
+        self.name = name
+        self.speed = speed
+
+    def getName(self):
+        return self.name
+
+    def getSpeed(self):
+        return self.speed
+
+## 변수 선언 부분
+car1, car2 = None, None
+
+# 메인 코드 부분
+car1 = Car("현대", 0)
+car2 = Car("기아", 40)
+
+print("%s의 현재 속도는 %d입니다." %(car1.getName(), car1.getSpeed()))
+print("%s의 현재 속도는 %d입니다." %(car2.getName(), car2.getSpeed()))
