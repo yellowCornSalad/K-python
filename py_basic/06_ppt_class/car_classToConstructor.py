c7d0640df2ab367f3110c63a80597b773bea3f53
junho <zk836566@gmail.com>
2022-03-14 11:00:02 +0900

    Update car_classToConstructor.py
    인스턴스 변수와 클래스 변수

--- py_basic/06_ppt_class/car_classToConstructor.py
+++ py_basic/06_ppt_class/car_classToConstructor.py
@@ -2,25 +2,22 @@
 
 
 class Car:
-    color = ""
-    speed = 0
+    color = "" # 인스턴스 변수
+    speed = 0 # 인스턴스 변수
+    count = 0 # 클래스 변수
 
-    def __init__(self, name, speed):
-        self.name = name
-        self.speed = speed
-
-    def getName(self):
-        return self.name
-
-    def getSpeed(self):
-        return self.speed
+    def __init__(self):
+        self.speed = 0
+        Car.speed = 0
 
 ## 변수 선언 부분
-car1, car2 = None, None
+myCar, myCar2 = None, None
 
 # 메인 코드 부분
-car1 = Car("현대", 0)
-car2 = Car("기아", 40)
+myCar1 = Car()
+myCar1.speed = 30
+print("자동차 1의 현재 속도는 %dkm, 생산된 자동차는 총 %d대입니다." %(myCar1.speed, myCar1.count))
 
-print("%s의 현재 속도는 %d입니다." %(car1.getName(), car1.getSpeed()))
-print("%s의 현재 속도는 %d입니다." %(car2.getName(), car2.getSpeed()))
+myCar2 = Car()
+myCar2.speed = 60
+print("자동차 2의 현재 속도는 %dkm, 생산된 자동차는 총 %d대입니다." %(myCar2.speed, myCar2.count))
